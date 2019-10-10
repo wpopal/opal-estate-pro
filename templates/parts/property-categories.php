bba159c41521862f403b1945bde3eead3c42d629
Hoang Huu <hoanghn@brainos.vn>
2019-10-10 09:33:38 +0700

    Update template.

--- templates/parts/property-categories.php
+++ templates/parts/property-categories.php
@@ -1,6 +1,10 @@
 <?php
 global $property;
 
-echo get_the_term_list( $property->get_id(), 'property_category', '<div class="property-categories-list"><span class="property-categories-list__label">' . esc_html__( 'Categories:', 'opalestate-pro'
-    ) .
-                                                                  '</span>', ', ', '</div>' );
+echo get_the_term_list(
+	$property->get_id(),
+	'property_category',
+	'<div class="property-categories-list"><span class="property-categories-list__label">' . esc_html__( 'Categories:', 'opalestate-pro' ) . '</span>',
+	', ',
+	'</div>'
+);
