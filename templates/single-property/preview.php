8d86aa767db201c2d81f6762a41fdd7b1d40543d
Hoang Huu <hoanghn@brainos.vn>
2019-10-31 14:11:53 +0700

    Fix single image size.

--- templates/single-property/preview.php
+++ templates/single-property/preview.php
@@ -1,8 +1,9 @@
 <div class="property-preview">
 	<?php
 	global $property;
-	$galleries  = $property->get_gallery();
-	$image_size = opalestate_get_option( 'opalestate_thumbnail_size', 'medium' );
+	$galleries         = $property->get_gallery();
+	$thumb_image_size  = opalestate_get_option( 'opalestate_thumbnail_size', 'medium' );
+	$single_image_size = opalestate_get_option( 'featured_image_size', 'full' );
 
 	if ( isset( $galleries ) && $galleries ):
 		?>
@@ -46,13 +47,13 @@
                 <div class="swiper-wrapper opalestate-gallery">
 					<?php if ( has_post_thumbnail() ): ?>
                         <div class="swiper-slide">
-							<?php the_post_thumbnail( opalestate_get_option( 'featured_image_size', 'full' ) ); ?>
+							<?php the_post_thumbnail( $single_image_size ); ?>
                         </div>
 					<?php endif; ?>
 					<?php if ( isset( $galleries ) && is_array( $galleries ) && $galleries ): ?>
 						<?php foreach ( $galleries as $key => $src ): ?>
                             <div class="swiper-slide">
-                                <img src="<?php echo esc_url( wp_get_attachment_image_url( $key, 'full' ) ); ?>" alt="gallery">
+                                <img src="<?php echo esc_url( wp_get_attachment_image_url( $key, $single_image_size ) ); ?>" alt="gallery">
                             </div>
 						<?php endforeach; ?>
 					<?php endif; ?>
@@ -69,22 +70,22 @@
 				$count_galleries = $count_galleries + count( $galleries );
 				?>
 				<?php if ( $count_galleries > $columns ) : ?>
-                    <div class="swiper-button-prev"> <i class="fas fa-angle-left"></i></div>
-                    <div class="swiper-button-next"> <i class="fas fa-angle-right"></i></div>
+                    <div class="swiper-button-prev"><i class="fas fa-angle-left"></i></div>
+                    <div class="swiper-button-next"><i class="fas fa-angle-right"></i></div>
 				<?php endif; ?>
 
                 <div class="swiper-wrapper">
 					<?php if ( has_post_thumbnail() ):
 						?>
                         <div class="swiper-slide">
-                            <div style="background-image:url('<?php echo wp_get_attachment_thumb_url( get_post_thumbnail_id(), $image_size ); ?>');" class="thumb-nav"></div>
+                            <div style="background-image:url('<?php echo wp_get_attachment_thumb_url( get_post_thumbnail_id(), $thumb_image_size ); ?>');" class="thumb-nav"></div>
                         </div>
 					<?php endif; ?>
 
 					<?php if ( isset( $galleries ) && is_array( $galleries ) && $galleries ): ?>
 						<?php foreach ( $galleries as $key => $src ): ?>
                             <div class="swiper-slide">
-                                <div style="background-image:url('<?php echo wp_get_attachment_image_url( $key, $image_size ); ?>');" class="thumb-nav"></div>
+                                <div style="background-image:url('<?php echo wp_get_attachment_image_url( $key, $thumb_image_size ); ?>');" class="thumb-nav"></div>
                             </div>
 						<?php endforeach; ?>
 					<?php endif; ?>
