46378081f5ba6777250c9ec2324f4b5ec4f8ac62
Hoang Huu <hoanghn@brainos.vn>
2019-12-09 15:04:48 +0700

    Fix order & package template. Tag 1.2.3

--- opal-estate-pro.php
+++ opal-estate-pro.php
@@ -3,7 +3,7 @@
  * Plugin Name: Opal Estate Pro
  * Plugin URI: https://wpdocs.gitbook.io/opal-estate/
  * Description: Opal Real Estate Plugin is an ideal solution and brilliant choice for you to set up a professional estate website.
- * Version: 1.2.2
+ * Version: 1.2.3
  * Author: WPOPAL
  * Author URI: http://www.wpopal.com
  * Requires at least: 4.6
@@ -151,7 +151,7 @@ if ( ! class_exists( 'OpalEstate' ) ) {
 		 */
 		public function __clone() {
 			// Cloning instances of the class is forbidden
-			_doing_it_wrong( __FUNCTION__, esc_html__( 'Cheatin&#8217; huh?', 'opalestate-pro' ), '1.2.2' );
+			_doing_it_wrong( __FUNCTION__, esc_html__( 'Cheatin&#8217; huh?', 'opalestate-pro' ), '1.2.3' );
 		}
 
 		/**
@@ -160,7 +160,7 @@ if ( ! class_exists( 'OpalEstate' ) ) {
 		public function setup_constants() {
 			// Plugin version
 			if ( ! defined( 'OPALESTATE_VERSION' ) ) {
-				define( 'OPALESTATE_VERSION', '1.2.2' );
+				define( 'OPALESTATE_VERSION', '1.2.3' );
 			}
 
 			// Plugin Folder Path
